9d092373dd700bd199e0e3a95038466908594d1c
Terror666 <ckuthulu@gmail.com>
2021-10-11 23:46:08 -0500

    reply on comments

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -5,7 +5,7 @@ const commentController = {
     addComment({ params, body }, res) {
         console.log(body);
         Comment.create(body)
-        .the(({ _id }) => {
+        .then(({ _id }) => {
             return Pizza.findOneAndUpdate(
                 { _id: params.pizzaId },
                 { $push: { comments: _id } },
@@ -41,6 +41,30 @@ const commentController = {
             }
             res.json(dbPizzaData);
         })
+        .catch(err => res.json(err));
+    },
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body} },
+            { new: true }
+        )
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    },
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+        .then (dbPizzaData => res.json(dbPizzaData))
         .catch(err => res.json(err));
     }
 };
